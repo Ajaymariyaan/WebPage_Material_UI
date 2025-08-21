081efaf0d13e1464d09bef0c9cbc23933ca276af
Ajay <ajayantony9296@gmail.com>
2025-08-21 14:17:50 +0530

    Nested Routing

---
+++ src/pages/Nest/Nested/Nested2Step.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Nested2Step(){
+
+
+    return(
+
+        <>
+        <h1>Nested  Two Step Folder</h1>
+        <h2> Nested Folder Like Nest-Nested-Nested2Step</h2>
+        </>
+    )
+}
+
+export default Nested2Step;
