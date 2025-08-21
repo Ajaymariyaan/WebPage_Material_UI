516f9ffccdd16b84acc0e6d59d48a2ddea2d9d98
Ajay <ajayantony9296@gmail.com>
2025-08-21 14:18:47 +0530

    Dynamic Routing

---
+++ src/pages/Dynamic/[id].js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+
+function DynamicId(){
+
+const router = useRouter();
+    const {id} = router.query;
+
+console.log("Dynamic Id",{id})
+    return(
+
+        <>
+        <h1> You Opened the id : {id} </h1>
+        
+        </>
+    )
+}
+export default DynamicId;
