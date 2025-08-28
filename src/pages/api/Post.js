7f7745d778744b104a051f7860ed95fce1b7e0a9
Ajay <ajayantony9296@gmail.com>
2025-08-28 14:47:16 +0530

    API Routes POST method

---
+++ src/pages/api/Post.js
@@ -0,0 +1,9 @@
+// fetch("/api/Get", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({ id: 1, name: "Ajay" })
+// })
+//   .then(res => res.json())
+//   .then(data => console.log(data));
+
+
